083da9d8c341fcfd248b23c6af89ca8919ea49e4
AsliddinBoriyev <asliddinboriyev824@gmail.com>
2022-08-03 11:13:00 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:animation/view_animation/hero_animation.dart';
+import 'package:animation/view_animation/springphsics_animation.dart';
+import 'package:flutter/material.dart';
+
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:  PhysicsCardDragDemo(),
+    );
+  }
+}
+
